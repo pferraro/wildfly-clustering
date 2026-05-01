83c94bb4d43d496185bdfd0c5b33dcaf3a08d816
Paul Ferraro <paul.ferraro@redhat.com>
2026-05-01 15:13:39 +0100

    Reimplement read-for-update semantics for use with transactional remote cache.

---
+++ cache/infinispan/remote/src/main/java/org/wildfly/clustering/cache/infinispan/remote/transaction/RemoteTransactionSerializationContextInitializer.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.wildfly.clustering.cache.infinispan.remote.transaction;
+
+import org.kohsuke.MetaInfServices;
+import org.wildfly.clustering.marshalling.protostream.AbstractSerializationContextInitializer;
+import org.wildfly.clustering.marshalling.protostream.Scalar;
+import org.wildfly.clustering.marshalling.protostream.SerializationContext;
+import org.wildfly.clustering.marshalling.protostream.SerializationContextInitializer;
+
+/**
+ * The serialization context initializer for this package.
+ * @author Paul Ferraro
+ */
+@MetaInfServices(SerializationContextInitializer.class)
+public class RemoteTransactionSerializationContextInitializer extends AbstractSerializationContextInitializer {
+
+	/**
+	 * Creates a serialization context initializer.
+	 */
+	public RemoteTransactionSerializationContextInitializer() {
+		// Do nothing
+	}
+
+	@Override
+	public void registerMarshallers(SerializationContext context) {
+		context.registerMarshaller(Scalar.ANY.toMarshaller(TransactionKey.class, TransactionKey::getId, TransactionKey::new));
+	}
+}
